c803fefbe20392554c0fe7a188bd1a5b15030fc2
ShoqZSon <heoningstim@web.de>
2025-11-04 12:11:27 +0100

    Übung 2 Aufgabenblatt

--- HttpSocketClient_URL.java
+++ HttpSocketClient_URL.java
@@ -1,11 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
-import java.io.*;
 
 public class HttpSocketClient_URL {
     public static void main(String[] args) throws IOException {
+        Proxy proxy = new Proxy(
+                Proxy.Type.HTTP,
+                new InetSocketAddress("127.0.0.1", 8888)
+        );
+
+
         URL url = new URL("http://localhost:8080/");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection(proxy);
         conn.setRequestMethod("GET");
 
         try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
